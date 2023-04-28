7e7554ebea4da17cb248f48265fbb0b42190e77e
Ali <alirezamonfared813@gmail.com>
2023-04-28 10:32:28 -0700

    ss

--- Assignment 5/ViewController.swift
+++ Assignment 5/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+  @IBOutlet weak var Thirdtextview: UITextView!
+  @IBOutlet weak var Secondtextview: UITextView!
+  @IBOutlet weak var imageview: UIImageView!
+  @IBOutlet weak var Firsttextview: UITextView!
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    imageview.backgroundColor = .white
+    imageview.layer.cornerRadius = imageview.frame.height / 2
+    Firsttextview.backgroundColor = .gray
+    Secondtextview.backgroundColor = .gray
+    Thirdtextview.backgroundColor = .gray
+    
+    
   }
+  
 
-
+  @IBAction func Login(_ sender: UIButton) {
+  }
+  
 }
 
